645d045d04abf9788fae564c8aff85ee349ada8d
Mohan <mohan@srv798713.hstgr.cloud>
2025-05-04 20:33:49 +0000

    first commit

---
+++ app/mohan/doctype/gik/gik.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, as and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class gik(Document):
+	pass
